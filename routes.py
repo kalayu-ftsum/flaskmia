76f5a16ea0ba0b851ba3c3bfd04684b81adab9ca
kalayu-ftsum <kalayuftsumm@gmail.com>
2021-03-02 18:44:25 +0300

    fa

--- routes.py
+++ routes.py
@@ -55,9 +55,9 @@ def login():
       user = User.query.filter_by(email=email).first()
       if user is not None and user.check_password(password):
         session['email'] = form.email.data
-        return redirect(url_for('home'))
+        return render_template("home.html")
       else:
-        return redirect(url_for('login'))
+        return render_template("login.html")
 
   elif request.method == 'GET':
     return render_template('login.html', form=form)
